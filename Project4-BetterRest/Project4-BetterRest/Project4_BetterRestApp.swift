620ca615753f0850d1fc1fc359f2a3348ee02182
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-11-23 19:42:41 +0800

    Day 26 - 28

---
+++ Project4-BetterRest/Project4-BetterRest/Project4_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project4_BetterRestApp.swift
+//  Project4-BetterRest
+//
+//  Created by Chloe Fermanis on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Project4_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
