99928da77572d78e7b2496ec581743b9c9304e08
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-11-29 19:22:12 +0800

    Day 36 - onDelete()

--- Project7-iExpense/Project7-iExpense/ContentView.swift
+++ Project7-iExpense/Project7-iExpense/ContentView.swift
@@ -7,59 +7,40 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-    
-}
-
-struct SecondView: View {
-    
-    @Environment(\.dismiss) var dismiss
-
-    let name: String
-    
-    
-    var body: some View {
-        
-        VStack {
-            Text("Hello, \(name)!")
-            Button("Dismiss") {
-                dismiss()
-            }
-        }
-        
-    }
-}
 
 struct ContentView: View {
     
-    @StateObject var user = User()
-    
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-                .padding()
+    
+        NavigationView {
             
-            TextField("First name", text: $user.firstName)
-                .textFieldStyle(.roundedBorder)
-
-            TextField("Last name", text: $user.lastName)
-                .textFieldStyle(.roundedBorder)
-            
-            Button("Show Sheet") {
-                showingSheet.toggle()
+            VStack {
+                
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
             }
-            .sheet(isPresented: $showingSheet) {
-                SecondView(name: "@twostraws")
+            .navigationTitle("onDelete")
+            .toolbar {
+                EditButton()
             }
-
         }
-        .padding()
+    
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
