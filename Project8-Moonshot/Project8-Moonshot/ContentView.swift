961b4fd4e94457d094884d55c004a613e658f2ea
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-12-01 19:11:56 +0800

    Day 39 - JSON

--- Project8-Moonshot/Project8-Moonshot/ContentView.swift
+++ Project8-Moonshot/Project8-Moonshot/ContentView.swift
@@ -7,21 +7,35 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
 struct ContentView: View {
     var body: some View {
         
-        NavigationView {
-            
-            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                NavigationLink {
-                    Text("Detail \(item)")
-                } label: {
-                    Text("Row \(item)")
-                        .padding()
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
-            .navigationTitle("SwiftUI")
             }
-            
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
         }
         
     }
