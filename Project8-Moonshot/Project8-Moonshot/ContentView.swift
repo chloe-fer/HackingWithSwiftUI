2800db61cd451dd7684bb6d8f149694eb85e80d6
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-12-01 19:06:07 +0800

    Day 39 - NavigationLink

--- Project8-Moonshot/Project8-Moonshot/ContentView.swift
+++ Project8-Moonshot/Project8-Moonshot/ContentView.swift
@@ -10,18 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        GeometryReader { geo in
-            Image("alice")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width*0.8, height: geo.size.height*0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
-
-                
-        }
+        NavigationView {
             
+            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                NavigationLink {
+                    Text("Detail \(item)")
+                } label: {
+                    Text("Row \(item)")
+                        .padding()
+                }
+            .navigationTitle("SwiftUI")
+            }
+            
+        }
+        
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
