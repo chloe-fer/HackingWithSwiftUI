7245d72509b10f1a70812af212479e37b2c9e840
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2021-12-05 15:41:44 +0800

    Day 40 - added extension for Colors

---
+++ Project8-Moonshot/Project8-Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Project8-Moonshot
+//
+//  Created by Chloe Fermanis on 2/12/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let description: String
+    
+}
