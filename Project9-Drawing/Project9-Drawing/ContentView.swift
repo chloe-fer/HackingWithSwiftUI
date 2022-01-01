00aa94b94ba8d3b1fcda9356f6f872006621886e
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2022-01-01 13:47:16 +0800

    Day 43 - Paths

---
+++ Project9-Drawing/Project9-Drawing/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Project9-Drawing
+//
+//  Created by Chloe Fermanis on 1/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Path { path in
+            
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            // path.closeSubpath()
+        }
+        // .fill(.blue)
+        // .stroke(.blue, lineWidth: 10)
+        // .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project9-Drawing/Project9-Drawing/Project9_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project9_DrawingApp.swift
+//  Project9-Drawing
+//
+//  Created by Chloe Fermanis on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Project9_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
