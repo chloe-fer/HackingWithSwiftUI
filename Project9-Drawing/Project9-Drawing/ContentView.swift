742830cbdb5f1bf812cf4cf2273b9ff0a4033736
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2022-01-03 13:06:44 +0800

    Day 45 - blur, blending

--- Project9-Drawing/Project9-Drawing/ContentView.swift
+++ Project9-Drawing/Project9-Drawing/ContentView.swift
@@ -9,18 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var colorCycle = 0.0
 
+    @State private var amount = 0.0
+    
     var body: some View {
       
         VStack {
             
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
-
-            Slider(value: $colorCycle)
+            ZStack {
+                
+                Image("alice")
+                    .resizable()
+                    .scaledToFill()
+                    .saturation(amount)
+                    .blur(radius: (1 - amount) * 20)
+                
+//                Circle()
+//                    .fill(Color(red: 1, green: 0, blue: 0))
+//                    .frame(width: 200 * amount)
+//                    .offset(x: -50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+//                    .fill(Color(red: 0, green: 1, blue: 0))
+//                    .frame(width: 200 * amount)
+//                    .offset(x: 50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+//                    .fill(Color(red: 0, green: 0, blue: 1))
+//                    .frame(width: 200 * amount)
+//                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 500)
+                        
+            Slider(value: $amount)
         }
-        
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
+
     }
     
 }
