1539460bb38bd83db51fefbf6f790139470978d3
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2022-01-08 17:27:56 +0800

    Day 49 - loading an image from a remote server

--- Project10-CupcakeCorner/Project10-CupcakeCorner/ContentView.swift
+++ Project10-CupcakeCorner/Project10-CupcakeCorner/ContentView.swift
@@ -7,59 +7,37 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    
-    var results: [Result]
-    
-}
-
-struct Result: Codable {
-    
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
 
 struct ContentView: View {
-    
-    @State private var results = [Result]()
-    
+        
     var body: some View {
         
-        List(results, id: \.trackId) { item in
-            
-            VStack(alignment: .leading) {
-                
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
-            }
-            
-        }
-        .task {
-            await loadData()
-        }
-    }
-    
-    func loadData() async {
+        // AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
         
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
+        // AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"))
+            // .resizable() doesn't work with async
+            // .frame(width: 200, height: 200) doesn't work with async or regular image
+
         
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
             
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
+            if let image = phase.image {
+                
+                image
+                    .resizable()
+                    .scaledToFit()
+                
+            } else if phase.error != nil {
+                Text("There was an error loading in the image.")
+            } else {
+                ProgressView()
             }
-        } catch {
-            print("Invalid data")
         }
+        .frame(width: 200, height: 200)
     }
 }
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
