4e2cfb8c3b27af336eecbfc9af04800b970038da
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2022-01-09 13:03:28 +0800

    Day 50 - address validation, disabled()

---
+++ Project10-CupcakeCorner/Project10-CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,23 @@
+//
+//  CheckoutView.swift
+//  Project10-CupcakeCorner
+//
+//  Created by Chloe Fermanis on 9/1/22.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
