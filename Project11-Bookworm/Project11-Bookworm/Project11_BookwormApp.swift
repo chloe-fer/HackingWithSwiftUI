b06f9d398b16587ffe5a6a457dcf82e6a1bf1f3d
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2022-01-11 20:50:52 +0800

    Day 53 - @Binding

---
+++ Project11-Bookworm/Project11-Bookworm/Project11_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project11_BookwormApp.swift
+//  Project11-Bookworm
+//
+//  Created by Chloe Fermanis on 11/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project11_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
