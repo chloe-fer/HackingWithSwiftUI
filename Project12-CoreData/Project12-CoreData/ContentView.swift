677a82dee2f9619022f28b29fa1164ba9e29388c
chloe-fer <62980379+chloe-fer@users.noreply.github.com>
2022-01-17 19:50:47 +0800

    Day 58 - filtering @FetchRequest using NSPredicate

--- Project12-CoreData/Project12-CoreData/ContentView.swift
+++ Project12-CoreData/Project12-CoreData/ContentView.swift
@@ -12,29 +12,44 @@ struct ContentView: View {
     
     @Environment(\.managedObjectContext) var moc
     
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    //@FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star Wars'")) var ships: FetchedResults<Ship>
+    
+    // @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
+    
+    // @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])) var ships: FetchedResults<Ship>
+
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "E")) var ships: FetchedResults<Ship>
+
 
     var body: some View {
         
         VStack {
             
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown Name")
             }
             
             Button("Add") {
-                let wizard = Wizard(context: moc)
-                wizard.name = "Harry Potter"
+                
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+
+                let ship4 = Ship(context: moc)
+                ship4.name = "executor"
+                ship4.universe = "Star Wars"
+                
+                try? moc.save()
             }
-            .padding()
             
-            Button("Save") {
-                do {
-                    try moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
         }
     
     }
